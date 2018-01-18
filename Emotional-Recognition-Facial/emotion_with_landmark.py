c94daf009afd85eb2048ad262138e79445e1faa3
ductai199x <taiducnguyen.drexel@gmail.com>
2018-01-18 00:07:04 -0500

    Facial Reg+Emotion Detect

---
+++ Emotional-Recognition-Facial/emotion_with_landmark.py
@@ -0,0 +1,186 @@
+import cv2
+import glob
+import random
+import math
+import numpy as np
+import dlib
+import itertools
+from sklearn.svm import SVC
+import os
+
+emotions = ["anger", "happy", "neutral", "sadness"] #Emotion list
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") #Or set this to whatever you named the downloaded file
+clf = SVC(kernel='linear', probability=True, tol=1e-3)#, verbose = True) #Set the classifier as a support vector machines with polynomial kernel
+
+faceDet = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+faceDet_two = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+faceDet_three = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+faceDet_four = cv2.CascadeClassifier("haarcascade_frontalface_alt_tree.xml")
+
+
+data = {} #Make dictionary for all values
+#data['landmarks_vectorised'] = []
+
+def get_files(emotion): #Define function to get file list, randomly shuffle it and split 80/20
+    training_files = glob.glob("dataset//%s//*" %emotion)
+    test_files = glob.glob("testset//*")
+    random.shuffle(training_files)
+    training = training_files[:int(len(training_files)*1.0)] #get first 80% of file list
+    # prediction = test_files[-int(len(test_files)*1.0)]
+    prediction = test_files[:int(len(test_files)*1.0)] #get last 20% of file list
+    return training, prediction
+
+def get_landmarks(image):
+    detections = detector(image, 1)
+    for k,d in enumerate(detections): #For all detected face instances individually
+        shape = predictor(image, d) #Draw Facial Landmarks with the predictor class
+        xlist = []
+        ylist = []
+        for i in range(1,68): #Store X and Y coordinates in two lists
+            xlist.append(float(shape.part(i).x))
+            ylist.append(float(shape.part(i).y))
+
+        xmean = np.mean(xlist)
+        ymean = np.mean(ylist)
+        xcentral = [(x-xmean) for x in xlist]
+        ycentral = [(y-ymean) for y in ylist]
+
+        landmarks_vectorised = []
+        for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
+            landmarks_vectorised.append(w)
+            landmarks_vectorised.append(z)
+            meannp = np.asarray((ymean,xmean))
+            coornp = np.asarray((z,w))
+            dist = np.linalg.norm(coornp-meannp)
+            landmarks_vectorised.append(dist)
+            landmarks_vectorised.append((math.atan2(y, x)*360)/(2*math.pi))
+
+        data['landmarks_vectorised'] = landmarks_vectorised
+    if len(detections) < 1:
+        data['landmarks_vestorised'] = "error"
+
+def make_sets():
+    training_data = []
+    training_labels = []
+    prediction_data = []
+    prediction_labels = []
+    for emotion in emotions:
+        print(" working on %s" %emotion)
+        training, prediction = get_files(emotion)
+        #Append data to training and prediction list, and generate labels 0-7
+        for item in training:
+            image = cv2.imread(item) #open image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #convert to grayscale
+            clahe_image = clahe.apply(gray)
+            get_landmarks(clahe_image)
+            if data['landmarks_vectorised'] == "error":
+                print("no face detected on this one")
+            else:
+                training_data.append(data['landmarks_vectorised']) #append image array to training data list
+                training_labels.append(emotions.index(emotion))
+
+        for item in prediction:
+            image = cv2.imread(item)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            clahe_image = clahe.apply(gray)
+            get_landmarks(clahe_image)
+            if data['landmarks_vectorised'] == "error":
+                print("no face detected on this one")
+            else:
+                prediction_data.append(data['landmarks_vectorised'])
+                prediction_labels.append(emotions.index(emotion))
+
+    return training_data, training_labels, prediction_data, prediction_labels
+
+def detect_resize_face():
+    files = glob.glob("testset//*")
+    filenumber = 0
+    for f in files:
+        frame = cv2.imread(f) #Open image
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Convert image to grayscale
+        #Detect face using 4 different classifiers
+        face = faceDet.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_two = faceDet_two.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_three = faceDet_three.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_four = faceDet_four.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+
+        #Go over detected faces, stop at first detected face, return empty if no face.
+        if len(face) == 1:
+            facefeatures = face
+        elif len(face_two) == 1:
+            facefeatures = face_two
+        elif len(face_three) == 1:
+            facefeatures = face_three
+        elif len(face_four) == 1:
+            facefeatures = face_four
+        else:
+            facefeatures = ""
+
+        #Cut and save face
+        for (x, y, w, h) in facefeatures: #get coordinates and size of rectangle containing face
+            print "face found in file: %s" %f
+            gray = gray[y:y+h, x:x+w] #Cut the frame to size
+
+            # try:
+            out = cv2.resize(gray, (350, 350)) #Resize face so all images have same size
+            cv2.imwrite("testset//%s.jpg" %filenumber, out) #Write image
+
+        os.remove(f)
+        filenumber += 1
+
+if __name__ == '__main__':
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow("test")
+    img_counter = 0
+    while True:
+        ret, frame = cam.read()
+        cv2.imshow("test", frame)
+        if not ret:
+            break
+        k = cv2.waitKey(1)
+
+        if k%256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            break
+        elif k%256 == 32:
+            # SPACE pressed
+            img_name = "testset//{}.png".format(img_counter)
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            img_counter += 1
+    cam.release()
+    cv2.destroyAllWindows()
+    detect_resize_face()
+    accur_lin = []
+    for i in range(0,1):
+        print("Making sets %s" %i)
+        training_data, training_labels, prediction_data, prediction_labels = make_sets()
+
+        npar_train = np.array(training_data) #Turn the training set into a numpy array for the classifier
+        npar_trainlabs = np.array(training_labels)
+        print("training SVM linear %s" %i) #train SVM
+        clf.fit(npar_train, npar_trainlabs)
+
+    print("getting accuracies %s" %i) #Use score() function to get accuracy
+    npar_pred = np.array(prediction_data)
+    #pred_lin = clf.score(npar_pred, prediction_labels)
+    prob = 100*np.array(clf.predict_proba(npar_pred))
+    prob_mean = np.mean(prob, axis=0)
+    prob_mean = np.around(prob_mean, decimals=2, out=None)
+    print prob_mean
+
+    user_emotion = raw_input("What was your feeling(anger, happy, neutral, sadness): ")
+    for emot in emotions:
+        if user_emotion == emot:
+            for f in glob.glob("testset//*"):
+                file = cv2.imread(f)
+                cv2.imwrite("dataset//%s//_%s" %(emot, os.path.basename(f)), file)
+                os.remove(f)
+
+    #     print "linear: ", pred_lin
+    #     accur_lin.append(pred_lin) #Store accuracy in a list
+    #
+    # print("Mean value lin svm: %s" %np.mean(accur_lin)) #FGet mean accuracy of the 10 runs
